eaf0c768dafc0f799024d24b302122ae9372f1e5
Thomas Moulia <jtmoulia@gmail.com>
2016-01-18 14:54:17 -0800

    theme change, about page update

--- jtmoulia-config.el
+++ jtmoulia-config.el
@@ -2,43 +2,52 @@
 
 ;;; Commentary:
 ;; Config for the 'org-page blog
+;;
+;; This config depends on `my-op', a custom project manager for org-page which
+;; is in my spacemacs config. Happily, no one will likely ever run this but me.
 
 ;;; Code:
 
 (require 'org-page)
+(require 'dash)  ;; req'd by org-page anyways!
 
-
-(setq op/repository-directory (file-name-directory
-                               (file-truename buffer-file-name))
-      op/site-domain "http://jtmoulia.pocketknife.io"
-      op/repository-org-branch "source"
-      op/repository-html-branch "master"
-      op/site-main-title "jtmoulia"
-      op/site-sub-title "public notebook 📓"
-      op/personal-github-link "http://github.com/jtmoulia"
-      op/theme 'mdo
-      op/personal-google-analytics-id "UA-60774978-1"
-      op/category-config-alist '(("blog" ;; this is the default configuration
-                                  :show-meta t
-                                  :show-comment t
-                                  :uri-generator op/generate-uri
-                                  :uri-template "/blog/%y/%m/%d/%t/"
-                                  :sort-by :date     ;; how to sort the posts
-                                  :category-index t) ;; generate category index or not
-                                 ("index"
-                                  :show-meta nil
-                                  :show-comment nil
-                                  :uri-generator op/generate-uri
-                                  :uri-template "/"
-                                  :sort-by :date
-                                  :category-index nil)
-                                 ("about"
-                                  :show-meta nil
-                                  :show-comment nil
-                                  :uri-generator op/generate-uri
-                                  :uri-template "/about/"
-                                  :sort-by :date
-                                  :category-index nil)))
+(let* ((root (file-name-directory (file-truename buffer-file-name)))
+       (theme-root-directory (file-name-as-directory (concat root "themes"))))
+  (add-to-list 'my-op-projects-alist
+               '(jtblog .
+                        ((org-html-toplevel-hlevel . 4)
+                         (op/repository-directory . ,root)
+                         (op/site-domain . "http://jtmoulia.pocketknife.io")
+                         (op/repository-org-branch . "source")
+                         (op/repository-html-branch . "master")
+                         (op/site-main-title . "jtmoulia")
+                         (op/site-sub-title . "public notebook 📓")
+                         (op/personal-github-link . "http://github.com/jtmoulia")
+                         (op/theme-root-directory . ,theme-root-directory)
+                         (op/theme . custom)
+                         (op/personal-google-analytics-id . "UA-60774978-1")
+                         (op/category-ignore-list . ("themes" "assets" "_build"))
+                         (op/category-config-alist . (("blog" ;; this is the default configuration
+                                                       :show-meta t
+                                                       :show-comment nil
+                                                       :uri-generator op/generate-uri
+                                                       :uri-template "/blog/%y/%m/%d/%t/"
+                                                       :sort-by :date     ;; how to sort the posts
+                                                       :category-index t) ;; generate category index or not
+                                                      ("index"
+                                                       :show-meta nil
+                                                       :show-comment nil
+                                                       :uri-generator op/generate-uri
+                                                       :uri-template "/"
+                                                       :sort-by :date
+                                                       :category-index nil)
+                                                      ("about"
+                                                       :show-meta nil
+                                                       :show-comment nil
+                                                       :uri-generator op/generate-uri
+                                                       :uri-template "/about/"
+                                                       :sort-by :date
+                                                       :category-index nil)))))))
 
 (provide 'jtmoulia-config)
 ;;; jtmoulia-config.el ends here
