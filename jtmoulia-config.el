74f974d09bc7d10e7ed6d750e962e39e6d9f9bea
Thomas Moulia <jtmoulia@gmail.com>
2018-11-23 01:17:17 -0800

    Fix org-page htmlize config setting

--- jtmoulia-config.el
+++ jtmoulia-config.el
@@ -20,7 +20,7 @@
                `(jtblog .
                         ((org-html-toplevel-hlevel . 4)
                          (org-confirm-babel-evaluate . nil)
-                         (op/highlight-render . 'htmlize)
+                         (op/highlight-render . htmlize)
                          (op/repository-directory . ,root)
                          (op/site-domain . "http://jtmoulia.pocketknife.io")
                          (op/repository-org-branch . "source")
